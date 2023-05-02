9c48ea0d8726213cf4fbac79917c546bb9cd7fbb
alexeykosh <alexeykochevoy@gmail.com>
2023-05-02 20:55:45 +0200

    organizing

---
+++ plots.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(ggalluvial)
+library(ggrepel)
+
+theme_set(theme_void())
+
+data <- read.csv('data/edges.csv')
+
+data %>%
+  filter(weight > 0) %>%
+  group_by(source) %>%
+  mutate(source_count = n()) %>%
+  filter(weight > 0) %>%
+  mutate(source = factor(source),
+         target = factor(target)) %>%
+  ggplot(aes(axis1 = source, axis2 = target, y = weight)) +
+  geom_alluvium(fill = "blue", width = 1/18) +
+  geom_stratum(width=1/18) +
+  geom_text_repel(stat = 'stratum', aes(label = ifelse(after_stat(x) == 1, as.character(after_stat(stratum)), NA)), 
+                  size = 3, direction = "y", nudge_x = -.2) +
+  geom_text_repel(stat = 'stratum', aes(label = ifelse(after_stat(x) == 2, as.character(after_stat(stratum)), NA)), 
+                  size = 3, direction = "y", nudge_x = .2) +
+  scale_x_discrete(limits = c('source', 'target')) +
+  theme(legend.position = 'none')
