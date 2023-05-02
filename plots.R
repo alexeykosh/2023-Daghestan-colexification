7900401d7abc5a7785c470d847ce087991b7b5bd
alexeykosh <alexeykochevoy@gmail.com>
2023-05-02 21:00:00 +0200

    changes fs

--- plots.R
+++ plots.R
@@ -17,8 +17,10 @@ data %>%
   geom_alluvium(fill = "blue", width = 1/18) +
   geom_stratum(width=1/18) +
   geom_text_repel(stat = 'stratum', aes(label = ifelse(after_stat(x) == 1, as.character(after_stat(stratum)), NA)), 
-                  size = 3, direction = "y", nudge_x = -.2) +
+                  size = 4.2, direction = "y", nudge_x = -.25) +
   geom_text_repel(stat = 'stratum', aes(label = ifelse(after_stat(x) == 2, as.character(after_stat(stratum)), NA)), 
-                  size = 3, direction = "y", nudge_x = .2) +
+                  size = 4.2, direction = "y", nudge_x = .25) +
   scale_x_discrete(limits = c('source', 'target')) +
   theme(legend.position = 'none')
+
+ggsave('figures/Fig1.pdf')
